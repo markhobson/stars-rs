2f549b6576f85b222f7d3e49b0f0fa1be3fb9f0c
Mark Hobson <markhobson@gmail.com>
2024-06-15 11:12:20 +0100

    Scroll single star across canvas

--- src/lib.rs
+++ src/lib.rs
@@ -22,10 +22,31 @@ pub unsafe extern fn go() {
 fn render_frame_safe(buffer: &mut [u32; WIDTH * HEIGHT]) {
     let f = FRAME.fetch_add(1, Ordering::Relaxed);
 
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
-            buffer[y * WIDTH + x] =
-                f.wrapping_add((x ^ y) as u32) | 0xFF_00_00_00;
+    clear_frame(buffer);
+
+    let x: usize = (f as usize) % WIDTH;
+    let y = 100;
+    render_star(buffer, x, y);
+}
+
+fn clear_frame(buffer: &mut [u32; WIDTH * HEIGHT]) {
+    for pixel in buffer.iter_mut() {
+        *pixel = 0xFF_00_00_00;
+    }
+}
+
+fn render_star(buffer: &mut [u32; WIDTH * HEIGHT], x0: usize, y0: usize) {
+    let colour = 0xFF_FF_FF_FF;
+    let size = 3;
+
+    for y in y0..min(y0 + size, HEIGHT) {
+        for x in x0..min(x0 + size, WIDTH) {
+            buffer[y * WIDTH + x] = colour;
         }
     }
+}
+
+// avoid std to minimise binary
+fn min(v1: usize, v2: usize) -> usize {
+    return if v1 < v2 { v1 } else { v2 };
 }
